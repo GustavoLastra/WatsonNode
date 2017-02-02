a847a5a6425d870a834e3b1f3e10d13f04cd3d39
GustavoLastra <gustavo.lastrac@gmail.com>
2017-02-02 17:58:56 +0100

    custom output

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-/**
-   Copyright 2014 AlchemyAPI
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 
 var express = require('express');
 var consolidate = require('consolidate');
@@ -47,13 +31,16 @@ app.get('/', example);
 var port = process.env.PORT || 3000;
 server.listen(port, function(){
 	console.log('Express server listening on port ' + port);
-	console.log('To view the example, point your favorite browser to: localhost:3000'); 
+	console.log('To view the example, point your favorite browser to: localhost:3000');
 });
 
 
-
 var demo_text = 'Yesterday dumb Bob destroyed my fancy iPhone in beautiful Denver, Colorado. I guess I will have to head over to the Apple Store and buy a new one.';
-var demo_url = 'http://www.npr.org/2013/11/26/247336038/dont-stuff-the-turkey-and-other-tips-from-americas-test-kitchen';
+//var demo_url = 'http://www.npr.org/2013/11/26/247336038/dont-stuff-the-turkey-and-other-tips-from-americas-test-kitchen';
+//var demo_url = 'https://www.tutorialspoint.com/nodejs/';
+
+
+var demo_url = 'https://apps.na.collabserv.com/wikis/home?lang=en-us#!/wiki/W9553d5e96be3_409d_a6f9_70406478d62e/page/App%20Dev%20for%20IBM%20Connections%20Cloud';
 var demo_html = '<html><head><title>Node.js Demo | AlchemyAPI</title></head><body><h1>Did you know that AlchemyAPI works on HTML?</h1><p>Well, you do now.</p></body></html>';
 
 
@@ -188,4 +175,3 @@ function image_keywords(req, res, output) {
 		res.render('example',output);
 	});
 }
-
